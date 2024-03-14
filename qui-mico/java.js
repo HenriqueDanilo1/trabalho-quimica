13df5f3916237becb432ae1645218dfe6ae80140
HenriqueDanilo1 <74213860+HenriqueDanilo1@users.noreply.github.com>
2024-03-13 22:47:20 -0300

    Add files via upload

---
+++ qui-mico/java.js
@@ -0,0 +1,129 @@
+function po(){
+    document.getElementById('pot').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xpot(){
+    document.getElementById('pot').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function pm(){
+    document.getElementById('pmt').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xpmt(){
+    document.getElementById('pmt').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function al(){
+    document.getElementById('alt').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xalt(){
+    document.getElementById('alt').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function pr(){
+    document.getElementById('prt').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xprt(){
+    document.getElementById('prt').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function n(){
+    document.getElementById('nt').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xnt(){
+    document.getElementById('nt').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function i(){
+    document.getElementById('it').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xit(){
+    document.getElementById('it').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function h(){
+    document.getElementById('ht').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xht(){
+    document.getElementById('ht').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function ca(){
+    document.getElementById('cat').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xcat(){
+    document.getElementById('cat').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function dy(){
+    document.getElementById('dyt').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xdyt(){
+    document.getElementById('dyt').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function lu(){
+    document.getElementById('lut').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xlut(){
+    document.getElementById('lut').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function mn(){
+    document.getElementById('mnt').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xmnt(){
+    document.getElementById('mnt').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function nb(){
+    document.getElementById('nbt').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xnbt(){
+    document.getElementById('nbt').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
+
+function fr(){
+    document.getElementById('frt').style.display="block"
+    document.getElementById('tp').style.filter="blur(3px)"
+    
+}
+function xfrt(){
+    document.getElementById('frt').style.display="none"
+    document.getElementById('tp').style.filter=""
+}
